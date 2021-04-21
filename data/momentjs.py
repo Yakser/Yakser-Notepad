a66cfad096a0fb6b358f718b649a41f63dda13b7
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-04-21 18:47:01 +0400

    Добавление пояснений и комментариев - momentjs.py

--- data/momentjs.py
+++ data/momentjs.py
@@ -3,13 +3,18 @@ from jinja2 import Markup
 
 
 class momentjs(object):
-    def __init__(self, timestamp):
+    """ Осуществляет работу с библиотекой MomentJs"""
 
+    def __init__(self, timestamp):
+        """
+        Приведение даты к объекту datetime
+        """
         year, month, day = map(int, timestamp.split()[0].split('-'))
         hour, minute = map(int, timestamp.split()[1].split(':'))
         self.timestamp = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
 
     def render(self, format):
+        """Добавляет в html код страницы отформатированную дату и время"""
         return Markup("""<script>moment.locale("ru");
                             document.write(moment(\"%s\").%s);
                         </script>"""
@@ -17,12 +22,15 @@ class momentjs(object):
                           self.timestamp.strftime("%Y-%m-%dT%H:%M:%S"), format))
 
     def format(self, fmt):
+        """Форматирует дату с помощью js-функции библиотеки MomentJs  - format(fmt)"""
         return self.render("format(\"%s\")" % fmt)
 
     def calendar(self):
+        """Форматирует дату с помощью js-функции библиотеки MomentJs  - calendar()"""
         return self.render("calendar()")
 
     def fromNow(self):
+        """Форматирует дату с помощью js-функции библиотеки MomentJs  - fromNow()"""
         return self.render("fromNow()")
 
 # momentjs
